c6e6e9a6b267409b1c53062f396d777b32f8439c
Kesavaa2 <148443860+Kesavaa2@users.noreply.github.com>
2024-08-07 22:08:05 +0530

    Create int_to_integer.sv

---
+++ Data Types/Casting data types/dynamic casting/int_to_integer.sv
@@ -0,0 +1,11 @@
+module int_to_integer;
+  
+  int int_data=53;
+  integer integer_data;
+  
+  initial begin
+    $cast(integer_data,int_data);
+    $display("integer_data=%d",integer_data);
+  end
+endmodule
+    
