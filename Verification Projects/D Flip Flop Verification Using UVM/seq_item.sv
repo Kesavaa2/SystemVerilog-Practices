d15410996dfb2d38477b34014e3ea2c3cc479408
Kesavaa2 <148443860+Kesavaa2@users.noreply.github.com>
2024-11-09 20:14:13 +0530

    Create seq_item.sv

---
+++ Verification Projects/D Flip Flop Verification Using UVM/seq_item.sv
@@ -0,0 +1,15 @@
+class dff_seq_item extends uvm_sequence_item;
+  `uvm_object_utils(dff_seq_item)
+  
+  rand bit rst;
+  rand bit d;
+  bit q;
+  
+  function new(string name="dff_seq_item");
+    super.new(name);
+    `uvm_info("sequence item class","constructor",UVM_MEDIUM)
+  endfunction
+  
+  constraint rst_en {rst dist {1:=10 , 0:=5};}
+  constraint d_en {d dist {1:=20 , 0:=5};}
+endclass
