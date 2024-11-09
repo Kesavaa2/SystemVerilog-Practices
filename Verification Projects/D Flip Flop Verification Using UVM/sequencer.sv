1bfe069bb1e17aa20d0bf6048bbf20ff2a62c66e
Kesavaa2 <148443860+Kesavaa2@users.noreply.github.com>
2024-11-09 20:22:55 +0530

    Update sequencer.sv

--- Verification Projects/D Flip Flop Verification Using UVM/sequencer.sv
+++ Verification Projects/D Flip Flop Verification Using UVM/sequencer.sv
@@ -3,7 +3,7 @@ class dff_sequencer extends uvm_sequencer#(dff_seq_item);
   
   function new(string name="dff_sequencer",uvm_component parent);
     super.new(name,parent);
-    `uvm_info("sequencer class","constructor",UVM_MEDIUM)
+    `uvm_info("SEQUENCER","constructor",UVM_MEDIUM)
   endfunction
   
   
