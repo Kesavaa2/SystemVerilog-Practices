0157bd953b5e83b62ad22fc818da0bcbfbe47591
Kesavaa2 <148443860+Kesavaa2@users.noreply.github.com>
2024-11-09 20:22:11 +0530

    Update driver.sv

--- Verification Projects/D Flip Flop Verification Using UVM/driver.sv
+++ Verification Projects/D Flip Flop Verification Using UVM/driver.sv
@@ -7,19 +7,19 @@ class dff_driver extends uvm_driver#(dff_seq_item);
   
   function new(string name ="dff_driver", uvm_component parent);
     super.new(name, parent);
-    `uvm_info("driver class", "constructor", UVM_MEDIUM)
+    `uvm_info("DRIVER", "constructor", UVM_MEDIUM)
   endfunction
   
    
   function void build_phase(uvm_phase phase);
     super.build_phase(phase);
     if(!uvm_config_db #(virtual dff_intf)::get(this, "", "intf",intf))
-      `uvm_fatal("no_inif in driver","virtual interface get failed from config db");
+      `uvm_fatal("DRIVER","virtual interface get failed from config db");
   endfunction
   
   task run_phase(uvm_phase phase);
     forever begin
-      `uvm_info("driver Class", "run_phase", UVM_MEDIUM)
+      `uvm_info("DRIVER", "run_phase", UVM_MEDIUM)
       tx=dff_seq_item::type_id::create("tx");
       
       seq_item_port.get_next_item(tx);
