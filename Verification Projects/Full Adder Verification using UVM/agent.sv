d41f2946297268c28e6096354a1be6c30f863230
Kesavaa2 <148443860+Kesavaa2@users.noreply.github.com>
2024-11-11 09:02:27 +0530

    Create agent.sv

---
+++ Verification Projects/Full Adder Verification using UVM/agent.sv
@@ -0,0 +1,24 @@
+class agent extends uvm_agent;
+  
+  `uvm_component_utils(agent);
+  driver drv;
+  sequencer seqr;
+  monitor mon;
+ 
+  function new(string name="agent",uvm_component parent=null);
+    super.new(name,parent);
+  endfunction
+  
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+      seqr=sequencer::type_id::create("seqr",this);
+    drv=driver::type_id::create("drv",this);
+    mon=monitor::type_id::create("mon",this);
+  endfunction
+  
+  
+  function void connect_phase(uvm_phase phase);
+    super.connect_phase(phase);
+        drv.seq_item_port.connect(seqr.seq_item_export);
+  endfunction
+endclass
